7c73dfd10aeb088178cc51b1be8b4b62408efb9e
Mercy Dan <mihretonline18@gmail.com>
2022-04-05 01:13:52 -0400

    activate API random quote

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-//console.log("connected!");
-let btn = document.getElementById ('btn');
-let output = document.getElementById ('output');
-let quotes =
-[
-    "God is good !",
-    "All is Well ",
-    "Now is the time",
-    "I am who I am because of him"
-];
-btn.addEvenetListener ('click', function(){
-    let randomQuote = quotes [Math.floor(Math.random()* quotes.length)]
-    output.innerHTML = randomQuote;
-})
+console.log("connected!");
+//let btn = document.getElementById ('btn');
+//let output = document.getElementById ('output');
+//let quotes =
+//[
+   
+//];
+//btn.addEvenetListener('click', function(){
+   // let randomQuote = quotes[Math.floor(Math.random()* quotes.length)]
+   // output.innerHTML = randomQuote;
+//})
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn = document.querySelector(" #btn")
+
+// randome quoute 
+function randomQuote(){
+    // feching random quoute 
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+    console.log(result);
+    quoteText.innerText = result.content;
+    authorName.innerText = result.author;
+    quoteBtn.innerText = "Press for a New Quote!";
+});
+}
+quoteBtn.addEventListener("click",randomQuote);
